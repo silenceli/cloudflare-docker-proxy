ea404de8fbbb8982c3d2914990057ae0fcf094c4
silenceli <lihao@cmss.chinamobile.com>
2024-07-10 14:33:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,11 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.aiclouldsz.icu": "https://registry-1.docker.io",
+  "quay.aiclouldsz.icu": "https://quay.io",
+  "gcr.aiclouldsz.icu": "https://k8s.gcr.io",
+  "k8s-gcr.aiclouldsz.icu": "https://k8s.gcr.io",
+  "ghcr.aiclouldsz.icu": "https://ghcr.io",
 };
 
 function routeByHosts(host) {
